0cea46d66d47d223b2de80fea5b0fc8016e34c17
clark128 <your_email@example.com>
2025-06-21 19:47:38 +0800

    Adding Fetch

--- react/main.js
+++ react/main.js
@@ -1,16 +1,30 @@
-const person1 = {
-  firstName : "Spongebob",
-  lastName : "Sqaurepants",
-  age : 30,
-  job : "Employed"
-}
+const btnPokemon = document.getElementById("btnSearch")
 
-const person2 = {
-  firstName : "Patrick",
-  lastName : "Star",
-  age : 34,
-}
 
-const {firstName,lastName,age, job = "Unemployed"}  = person2
+btnPokemon.addEventListener("click",async function result(){
+  try{ const search = document.getElementById("search").value.toLowerCase()
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${search}`)
+      if(!response.ok){
+        const err = document.getElementById("error").textContent = "No Found"
+        const text = document.getElementById("txt").textContent = ""
+        const imgElement = document.getElementById("imgPokemon").style.display = "none"
+        return {err,text,imgElement}
+      } else{
+        document.getElementById("error").textContent = ""
+      }
+    const data = await response.json()
+    const pokemonSprite =  data.sprites.front_default;
+    const pokemonName = data.name
+    const imgElement = document.getElementById("imgPokemon")
 
-console.log(job)
+    imgElement.src = pokemonSprite
+    imgElement.style.display = "block"
+
+    const names = document.getElementById("txt").textContent = `Pokemon Name : ${pokemonName}`
+    return names
+  }
+  catch(error){
+    console.warn(error)
+  }
+
+})
